26adce036d303f3ce1e4ce09736e82e357250292
martiniclayton <68083141+martiniclayton@users.noreply.github.com>
2026-08-12 19:24:17 -0300

    ajuste nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 const header = document.getElementById("header");
+const overlay = document.querySelector(".overlay");
+window.addEventListener("scroll", atualizarNav)
+atualizarNav();
+
 
 function navTemaBlur(){
     header.classList.add("scroll");
@@ -8,17 +12,20 @@ function navTemaTrasparente(){
     header.classList.remove("scroll");
 }
 
-window.addEventListener("scroll", ()=>{
-    if(window.scrollY > 50){
-        navTemaBlur()
-    } else{
-        navTemaTrasparente();
-    }
-})
+function atualizarNav(){
+        if(window.scrollY > 50){
+            navTemaBlur()
+        } else{
+            navTemaTrasparente();
+        }
+}
+
 
 
 const nav = document.getElementById("menuNav");
 
+const btnToogler = document.getElementById("btn-toggle");
+btnToogler.addEventListener("click", abrirMenu);
 
 window.addEventListener("click", (e)=>{
     if(!nav.contains(e.target) && !btnToogler.contains(e.target)){
@@ -27,26 +34,24 @@ window.addEventListener("click", (e)=>{
 })
 
 
-
-const btnToogler = document.getElementById("btn-toggle");
-
-
-btnToogler.addEventListener("click", abrirMenu);
-
-
 const iconeFecharToggler = document.getElementById("btn-fecharMenu");
 
 iconeFecharToggler.addEventListener("click", ()=>{
     fecharMenu();
 })
 function abrirMenu(){
+    navTemaTrasparente();
     nav.classList.add("show");
     header.classList.add("close");
     document.body.classList.add("travar")
+    overlay.classList.add("ativo");
 }
 
 function fecharMenu(){
+    atualizarNav();
     nav.classList.remove("show");
     header.classList.remove("close");
     document.body.classList.remove("travar")
+    overlay.classList.remove("ativo");
+
 }
